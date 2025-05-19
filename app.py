482e177f807af6208afa06a3bf757666c9e027c8
ViuRemix/ <worksor.78@gmail.com>
2025-05-19 16:56:36 +0700

    add all folder

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+app.secret_key = 'secret'
+db = SQLAlchemy(app)
+
+# Khai báo model
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    author = db.Column(db.String(100))
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+
+# Tạo giao diện admin
+admin = Admin(app, name="Quản lý thư viên", template_mode="bootstrap3")
+admin.add_view(ModelView(Book, db.session))
+admin.add_view(ModelView(User, db.session))
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+
+
+
---
+++ test_app.py
@@ -0,0 +1,3 @@
+def test_example():
+    # Một smoke test đơn giản để đảm bảo pytest chạy ổn
+    assert 1 + 1 == 2
