0da895f3cc2d7acb73b7a6e6fc2199515dcd075b
ViuRemix/ <worksor.78@gmail.com>
2025-05-20 22:54:54 +0700

    delploy kubernetes

--- app.py
+++ app.py
@@ -19,15 +19,16 @@ class User(db.Model):
     username = db.Column(db.String(50))
 
 # Tạo giao diện admin
-admin = Admin(app, name="Quản lý thư viên", template_mode="bootstrap3")
+admin = Admin(app, name="Quản lý thư viện", template_mode="bootstrap3")
 admin.add_view(ModelView(Book, db.session))
 admin.add_view(ModelView(User, db.session))
+
+# Thêm route chính trang chủ
+@app.route('/')
+def index():
+    return '<h1>Chào mừng đến với hệ thống quản lý thư viện!</h1><p>Truy cập <a href="/admin">/admin</a> để vào trang quản trị.</p>'
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
-
-
-
